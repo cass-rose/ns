594b70b5f258192bda9c876045b09a7630b9a202
cass-rose <110793331+cass-rose@users.noreply.github.com>
2022-08-07 21:06:23 -0600

    Add files via upload

--- getnats.py
+++ getnats.py
@@ -3,21 +3,30 @@ import requests
 import xmltodict
 import re
 
-def getDict():
+def getDict(headers={'User-agent': 'Synstylae: (https://github.com/cass-rose/ns), nation: https://www.nationstates.net/nation=synstylae'}):
     headers = {
-        'User-agent': 'Synstylae(developer: +https://github.com/cass-rose/ns)'
+        'User-agent': 'Synstylae: (https://github.com/cass-rose/ns), nation: https://www.nationstates.net/nation=synstylae'
     }
 
     response = requests.get(
-        "https://www.nationstates.net/cgi-bin/api.cgi?q=happenings;filter=founding;limit=8", headers=headers
+        "https://www.nationstates.net/cgi-bin/api.cgi?q=happenings;filter=founding", headers=headers
     )
-    # f=open('response.txt','w')
-    # f.write(response.text)
-    # f.close()
     treeDict=xmltodict.parse(response.content)
-    # print(treeDict)
     natHapps=treeDict['WORLD']['HAPPENINGS']['EVENT']
+    d=0
+    nations=[]
+    temp=''
     for hap in natHapps:
-        print(re.findall('\@@(.*?)\@',hap['TEXT'])[0]+',')
+        d+=1
+        temp+=re.findall('\@@(.*?)\@',hap['TEXT'])[0]+','
+        if (d%8)==0:
+            nations.append(temp)
+            temp=''
 
-getDict()
+    return nations
+
+def main():
+    nats=getDict()
+    print(nats)
+
+main()
